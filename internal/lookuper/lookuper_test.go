4a57ac143a826db9cc3011733e7626ffe2c220c2
pabateman <ronofeedback@gmail.com>
2024-12-16 21:20:58 +0700

    Extend lookuper tests

--- internal/lookuper/lookuper_test.go
+++ internal/lookuper/lookuper_test.go
@@ -15,21 +15,23 @@ var (
 	expectedContentDirectory = "../../testdata/lookuper/expected"
 )
 
-func getStringFromFile(path string) (string, error) {
-	file, err := os.ReadFile(path)
-	if err != nil {
-		return "", fmt.Errorf("error while opening file: %v", err)
+func getFilesAsString(paths ...string) ([]string, error) {
+	result := make([]string, 0)
+
+	for _, path := range paths {
+
+		file, err := os.ReadFile(path)
+		if err != nil {
+			return nil, fmt.Errorf("error while opening file: %v", err)
+		}
+		result = append(result, string(file))
 	}
 
-	return string(file), nil
+	return result, nil
 }
 
 func TestTaskBasic(t *testing.T) {
-	output, err := os.CreateTemp(outputDirectory, "lookuper-output")
-	require.Nil(t, err)
-
-	defer output.Close()
-	defer os.Remove(output.Name())
+	output := path.Join(outputDirectory, "actual-basic.txt")
 
 	settings := &settings{
 		LookupTimeout: "15s",
@@ -40,17 +42,144 @@ func TestTaskBasic(t *testing.T) {
 		Files: []string{
 			path.Join(listsDirectory, "basic.lst"),
 		},
-		Output: output.Name(),
+		Output: output,
 	}
 
-	err = performTask(task, settings)
+	err := performTask(task, settings)
 	require.Nil(t, err)
 
-	expected, err := getStringFromFile(path.Join(expectedContentDirectory, "basic.txt"))
+	expected, err := getFilesAsString(path.Join(expectedContentDirectory, "basic-list.txt"))
 	require.Nil(t, err)
 
-	actual, err := getStringFromFile(output.Name())
+	actual, err := getFilesAsString(output)
 	require.Nil(t, err)
 
 	require.Equal(t, expected, actual)
+
+	err = os.Remove(output)
+	require.Nil(t, err)
+}
+
+func TestTaskMultiple(t *testing.T) {
+	outputs := []string{
+		path.Join(outputDirectory, "actual-multiple-01.txt"),
+		path.Join(outputDirectory, "actual-multiple-02.txt"),
+	}
+
+	inputs := []string{
+		path.Join(listsDirectory, "basic.lst"),
+		path.Join(listsDirectory, "multiple-01.lst"),
+		path.Join(listsDirectory, "multiple-02.lst"),
+	}
+
+	expecteds := []string{
+		path.Join(expectedContentDirectory, "multiple-01.txt"),
+		path.Join(expectedContentDirectory, "multiple-02.txt"),
+	}
+
+	config := &config{
+		Settings: &settings{
+			LookupTimeout: "15s",
+			Fail:          false,
+		},
+
+		Tasks: []task{
+			{
+				Files:  inputs[:2],
+				Output: outputs[0],
+			},
+			{
+				Files:  inputs[2:],
+				Output: outputs[1],
+			},
+		},
+	}
+
+	err := walkTasks(config)
+	require.Nil(t, err)
+
+	actual, err := getFilesAsString(outputs...)
+	require.Nil(t, err)
+
+	expected, err := getFilesAsString(expecteds...)
+	require.Nil(t, err)
+
+	require.Equal(t, expected, actual)
+
+	config.Settings.Fail = true
+	err = walkTasks(config)
+	require.NotNil(t, err)
+
+	for _, output := range outputs {
+		err = os.Remove(output)
+		require.Nil(t, err)
+	}
+}
+
+func TestTaskFails(t *testing.T) {
+	output := path.Join(outputDirectory, "actual-multiple-02.txt")
+	input := path.Join(listsDirectory, "multiple-02.lst")
+
+	config := &config{
+		Settings: &settings{
+			LookupTimeout: "15s",
+			Fail:          false,
+		},
+
+		Tasks: []task{
+			{
+				Files:  []string{input},
+				Output: output,
+			},
+		},
+	}
+
+	err := walkTasks(config)
+	require.Nil(t, err)
+
+	expected, err := getFilesAsString(path.Join(expectedContentDirectory, "multiple-02.txt"))
+	require.Nil(t, err)
+
+	actual, err := getFilesAsString(output)
+	require.Nil(t, err)
+
+	require.Equal(t, expected, actual)
+
+	config.Settings.Fail = true
+	err = walkTasks(config)
+	require.NotNil(t, err)
+
+	config.Settings.Fail = false
+	err = walkTasks(config)
+	require.Nil(t, err)
+
+	config.Settings.LookupTimeout = "1"
+	err = walkTasks(config)
+	require.NotNil(t, err)
+
+	config.Settings.LookupTimeout = "15s"
+	err = walkTasks(config)
+	require.Nil(t, err)
+
+	inputNxdomain := path.Join(listsDirectory, "inputnxdomain.lst")
+	outputNxdomainPath := path.Join(outputDirectory, "outputnxdomain.lst")
+	expectedNxdomainPath := path.Join(expectedContentDirectory, "stub")
+
+	config.Tasks[0].Files = []string{inputNxdomain}
+	config.Tasks[0].Output = outputNxdomainPath
+
+	err = walkTasks(config)
+	require.Nil(t, err)
+
+	actualNxdomain, err := getFilesAsString(outputNxdomainPath)
+	require.Nil(t, err)
+
+	expectedNxdomain, err := getFilesAsString(expectedNxdomainPath)
+	require.Nil(t, err)
+
+	require.Equal(t, expectedNxdomain, actualNxdomain)
+
+	config.Settings.Fail = true
+	err = walkTasks(config)
+	require.NotNil(t, err)
 }
